d3f4334e8ccac30d04b209db496d9a2df5642680
unknown <moonsong98@postech.ac.kr>
2023-03-19 19:18:57 +0900

    Implement ImmediateGenerator

--- Lab02/ImmediateGenerator.v
+++ Lab02/ImmediateGenerator.v
@@ -1,29 +1,27 @@
 `include "opcodes.v"
-`include "alu_opcodes.v"
 
 module ImmediateGenerator(input [31:0] instr,   
-                          output [31:0] imm_gen_out); 
+                          output reg [31:0] imm_gen_out); 
     
     always @(*) begin
         case (instr)
             // I-type
             // ADDI, XORI, ORI, AND, SLLI, SRLI
-            0010011: imm_gen_out = {21{instr[31]}, instr[30:20]};
+            0010011: imm_gen_out = { {21{instr[31]}}, instr[30:20] };
             // LW
-            0000011::
+            0000011: imm_gen_out = { {21{instr[31]}}, instr[30:20] };
             // JALR
-            1100111:
+            1100111: imm_gen_out = { {21{instr[31]}}, instr[30:20] };
             // S-type
             // SW
-            0100011:
+            0100011: imm_gen_out = { {21{instr[31]}}, instr[30:25], instr[11:7] };
             // SB-type
             //BEQ, BNE, BLT, BGE
-            1100111:
+            1100111: imm_gen_out = { {20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 0 };
             // UJ-type
             //JAL
-            1101111:
-            default : 
+            1101111: imm_gen_out = { {12{instr[31]}}, instr[19:12], instr[20], instr[30:25], instr[24:21], 0 };
+            default : imm_gen_out = 0; // NO CASE
         endcase
     end                  
 endmodule
-                //extended[15:0] <= { {8{extend[7]}}, extend[7:0] }; 
