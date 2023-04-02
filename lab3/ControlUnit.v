16cb46135d7223cb09e64d45e312c1b64c5c3d85
kookhh0827 <kookhh0827@gmail.com>
2023-04-02 21:07:26 +0900

    creates Control unit's skeleton codes

--- lab3/ControlUnit.v
+++ lab3/ControlUnit.v
@@ -1,3 +1,5 @@
+`include "states.v"
+
 module ControlUnit(input reset,
                    input clk,
                    input [6:0] opcode,
@@ -16,4 +18,63 @@ module ControlUnit(input reset,
                    output RegWrite,
                    output is_ecall);
     
+    /***** Register declarations *****/
+    // current micro program counter                
+    reg [4:0] current_state;
+    
+    /***** Wire declarations *****/
+    // for program counter update
+    wire [4:0] next_state;
+    
+    // for address select logic
+    wire [4:0] cs_plus_1 = current_state + 1'b1;
+    wire [1:0] AddrCtl;
+    wire ns_IF1;
+    
+    // ---------- EcallChcker checks whether an instruction is ecall or not during ID state ----------
+    EcallChecker EC(
+        .current_state(current_state),  // input
+        .opcode(opcode),                // input
+        .is_ecall(is_ecall)             // output
+    );
+    
+    // ---------- MicroProgram counter select logic ----------
+    AddressSelector ASL(
+        .opcode(opcode),         // input
+        .cs_plus_1(cs_plus_1),   // input
+        .AddrCtl(AddrCtl),       // input
+        .alu_bcond(alu_bcond),   // input
+        .ns_IF1(ns_IF1),         // input
+        .is_ecall(is_ecall),     // input
+        .next_state(next_state)  // output
+    );
+    
+    // ---------- calculates control bits based on a current state ----------
+    MicroStorage MS(
+        .current_state(current_state),   // input
+        .AddrCtl(AddrCtl),               // output
+        .ns_IF1(ns_IF1),                 // output
+        .PCWriteNotCond(PCWriteNotCond), // output
+        .PCWrite(PCWrite),               // output
+        .IorD(IorD),                     // output
+        .MemRead(MemRead),               // output
+        .MemWrite(MemWrite),             // output
+        .MemtoReg(MemtoReg),             // output
+        .IRWrite(IRWrite),               // output
+        .PCSource(PCSource),             // output
+        .ALUOp(ALUOp),                   // output
+        .ALUSrcA(ALUSrcA),               // output
+        .ALUSrcB(ALUSrcB),               // output
+        .RegWrite(RegWrite),             // output
+    );
+    
+    // ---------- program counter update logic ----------
+    always @(*) begin
+        if (reset) begin
+            current_state <= `IF1;
+        end
+        else begin
+            current_state <= next_state;
+        end
+    end
 endmodule
---
+++ lab3/states.v
@@ -0,0 +1,25 @@
+// states for multi cycle cpus definition
+
+`define IF1     5'd0 
+`define IF2     5'd1 
+`define IF3     5'd2
+`define IF4     5'd3
+`define ID      5'd4
+`define EX1_R   5'd5
+`define EX2_R   5'd6
+`define EX1_I   5'd7
+`define EX2_I   5'd8
+`define EX_LW   5'd9
+`define EX1_B   5'd10
+`define EX2_B   5'd11
+`define MEM1_WR 5'd12
+`define MEM2_WR 5'd13 
+`define MEM3_WR 5'd14
+`define MEM4_WR 5'd15
+`define MEM1_RD 5'd16
+`define MEM2_RD 5'd17
+`define MEM3_RD 5'd18
+`define MEM4_RD 5'd19 
+`define WB_R    5'd20 
+`define WB_M    5'd21
+`define WB_J    5'd22
