58b593a7e3b82c2c09682bcbb7cd96b358bbe33d
kookhh0827 <kookhh0827@gmail.com>
2023-04-17 17:28:56 +0900

    changes don't care bits to 0s

--- lab3/MicroStorage.v
+++ lab3/MicroStorage.v
@@ -19,55 +19,55 @@ module MicroStorage(input [4:0] current_state,
         case(current_state)
         `IF: begin
             AddrCtl = 2'b11;
-            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b00010x1xxxxxx0;
+            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b00010010000000;
         end
         `ID: begin
             AddrCtl = 2'b01;
-            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b00x00x00000010;
+            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b00000000000010;
         end
         `EX_R: begin
             AddrCtl = 2'b10;
-            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b00x00x0x101000;
+            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b00000000101000;
         end
         `EX_I: begin
             AddrCtl = 2'b10;
-            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b00x00x0x111100;
+            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b00000000111100;
         end
         `EX_LW: begin
             AddrCtl = 2'b10;
-            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b00x00x0x001100;
+            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b00000000001100;
         end
         `EX1_B: begin
             AddrCtl = 2'b11;
-            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b10x00x01011000;
+            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b10000001011000;
         end
         `EX2_B: begin
             AddrCtl = 2'b00;
-            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b01x00x00000100;
+            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b01000000000100;
         end
         `MEM_WR: begin
             AddrCtl = 2'b00;
-            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b01101x00000010;
+            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b01101000000010;
         end
         `MEM_RD: begin
             AddrCtl = 2'b11;
-            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b00110x0x001100;
+            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b00110000001100;
         end
         `WB_M: begin
             AddrCtl = 2'b00;
-            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b01x00100000011;
+            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b01000100000011;
         end
         `WB_R: begin
             AddrCtl = 2'b00;
-            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b01x00000000011;
+            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b01000000000011;
         end
         `WB_JAL: begin
             AddrCtl = 2'b00;
-            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b01x00000000101;
+            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b01000000000101;
         end
         `WB_JALR: begin
             AddrCtl = 2'b00;
-            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b01x00000001101;
+            {PCWriteNotCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, PCSource, ALUOp, ALUSrcA, ALUSrcB, RegWrite} = 14'b01000000001101;
         end
         default: begin
             AddrCtl = 2'b00;
