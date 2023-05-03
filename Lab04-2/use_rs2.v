b8478579b20d4e4ce72de11c428da3db2e00b10e
kookhh0827 <kookhh0827@gmail.com>
2023-05-03 23:43:56 +0900

    complets always not taken version

---
+++ Lab04-2/use_rs2.v
@@ -0,0 +1,12 @@
+`include "opcodes.v"
+
+module Use_RS2(
+    input [6:0] opcode,
+    input [4:0] rs2,
+    output reg use_rs2
+    );
+    
+    always @(*) begin
+        use_rs2 = (opcode == `ARITHMETIC || opcode == `STORE || opcode == `BRANCH) && rs2 != 5'b0;
+    end
+endmodule
